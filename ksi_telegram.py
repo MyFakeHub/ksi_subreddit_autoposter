1570237ee629ae4189b42c7d52824d9fa94d01de
fake <fakeanddisposable1@gmail.com>
2021-07-06 14:52:10 +0000

    v1.0

--- ksi_telegram.py
+++ ksi_telegram.py
@@ -81,13 +81,10 @@ while True:
                 message = template.format(title=title, link=link, user=user)
 
                 print("Posting {}".format(link))
-                print("Channel: {}".format(channel))
-                print("Message: {}".format(message))
                 bot.sendPhoto(chat_id=channel, photo=submission.url, caption=message)
                 # bot.sendMessage(chat_id=channel, parse_mode=telegram.ParseMode.HTML, text=message)
-                print("writing")
                 write_submissions(submission.id)
-                sleep(600)
+                sleep(60)
             except Exception as e:
                 print("Exception: Error parsing {}".format(link))
                 print(e)
